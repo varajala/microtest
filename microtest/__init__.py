6cb7b2e83c651e4d05194e8bf01d260e887ccaa1
varajala <rajalainen.valtteri@gmail.com>
2021-05-26 12:00:28 +0300

    Added new find_and_exec - entrypoint for programmatically execute other tests

--- microtest/__init__.py
+++ microtest/__init__.py
@@ -1,5 +1,42 @@
 """
 Author: Valtteri Rajalainen
-Edited: 4.3.2021
+Edited: 26.5.2021
 """
+
+import os
+import traceback
+import microtest.runner as runner
+import microtest.scanner as scanner
+import microtest.core as core
 from microtest.api import *
+
+
+def run_from_commandline(args):
+    #handle flags etc...
+    cwd = os.getcwd()
+    path = cwd
+    if args:
+        path = args.pop(-1)
+        if not os.path.exists(path):
+            sys.stderr.write(f'Invalid path: {path}.\n')
+            return
+        
+        if not os.path.isabs(path):
+            path = os.path.join(cwd, path)
+    
+    if os.path.isfile(path):
+        core.logger.log_start_info()
+        runner.run((path,), lambda path, e, et, tb: traceback.print_exception(e, et, tb))
+        return
+
+    find_and_exec(path)
+
+
+def find_and_exec(path):
+    modules = scanner.find_tests(path)
+    if not modules:
+        sys.stdout.write(f'No modules found.\n')
+        return
+    
+    core.logger.log_start_info()
+    runner.run(modules, core.register_module_exec_error, core.register_module)
--- microtest/__main__.py
+++ microtest/__main__.py
@@ -2,44 +2,13 @@
 Commandline entrypoint for testing.
 
 Author: Valtteri Rajalainen
-Edited: 3.5.2021
+Edited: 26.5.2021
 """
 
 import sys
-import os
-import traceback
-import microtest.runner as runner
-import microtest.scanner as scanner
-import microtest.core as core
-
-
-def main(args):
-   #handle flags etc...
-   cwd = os.getcwd()
-   path = cwd
-   if args:
-      path = args.pop(-1)
-      if not os.path.exists(path):
-         sys.stderr.write(f'Invalid path: {path}.\n')
-         return
-      
-      if not os.path.isabs(path):
-         path = os.path.join(cwd, path)
-   
-   if os.path.isfile(path):
-      core.logger.log_start_info()
-      runner.run((path,), lambda path, e, et, tb: traceback.print_exception(e, et, tb))
-      return
-
-   modules = scanner.find_tests(path)
-   if not modules:
-      sys.stdout.write(f'No modules found.\n')
-      return
-   
-   core.logger.log_start_info()
-   runner.run(modules, core.register_module_exec_error, core.register_module)
+from microtest import run_from_commandline
 
 
 if __name__ == '__main__':
-   main(sys.argv[1:])
+   run_from_commandline(sys.argv[1:])
 
