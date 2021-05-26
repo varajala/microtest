b7b4d5db06c5c33017d302e2a3539efbea81bd8a
varajala <rajalainen.valtteri@gmail.com>
2021-05-26 12:27:51 +0300

    Added context object to hold state between executed modules

--- microtest/__init__.py
+++ microtest/__init__.py
@@ -4,11 +4,26 @@ Edited: 26.5.2021
 """
 
 import os
+import sys
 import traceback
 import microtest.runner as runner
 import microtest.scanner as scanner
 import microtest.core as core
 from microtest.api import *
+
+
+class __Context:
+    def __init__(self):
+        object.__setattr__(self, 'data', dict())
+
+    def __getattribute__(self, attr):
+        return object.__getattribute__(self, 'data').get(attr, None)
+
+    def __setattr__(self, attr, value):
+        object.__getattribute__(self, 'data').__setitem__(attr, value)
+
+
+context = __Context()
 
 
 def run_from_commandline(args):
