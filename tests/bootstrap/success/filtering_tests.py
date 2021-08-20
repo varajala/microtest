1d7155fc81729bd5db850cb6dc20150bcc8c8ebd
varajala <rajalainen.valtteri@gmail.com>
2021-08-20 10:35:28 +0300

    Relocated filtering assets to assets directory

--- tests/bootstrap/success/filtering_tests.py
+++ tests/bootstrap/success/filtering_tests.py
@@ -5,7 +5,7 @@ import os
 import tempfile
 
 
-def run_microtest(directory: str) -> str:
+def run_microtest_as_module(directory: str) -> str:
     cmd = [sys.executable, '-m', 'microtest', directory]
     stream = tempfile.TemporaryFile(mode='w+')
     
@@ -22,6 +22,7 @@ def join_asset_path(*args):
     path = os.path.dirname(os.path.abspath(__file__))
     path = os.path.dirname(path)
     path = os.path.dirname(path)
+    path = os.path.join(path, 'assets')
     for name in args:
         path = os.path.join(path, name)
     return path
@@ -30,14 +31,14 @@ def join_asset_path(*args):
 
 @microtest.test
 def test_exclude_modules():
-    output = run_microtest(join_asset_path('module_filtering', 'exclude_modules'))
+    output = run_microtest_as_module(join_asset_path('module_filtering', 'exclude_modules'))
     assert 'config executed' in output
     assert 'run_this_test' in output
 
 
 @microtest.test
 def test_only_modules():
-    output = run_microtest(join_asset_path('module_filtering', 'only_modules'))
+    output = run_microtest_as_module(join_asset_path('module_filtering', 'only_modules'))
     assert 'config executed' in output
     assert 'run_this_test' in output
     assert 'exec_this_test' in output
@@ -46,7 +47,7 @@ def test_only_modules():
 @microtest.test
 def test_exclude_groups():
     os.environ['MICROTEST_ENTRYPOINT'] = 'exclude_slow.py'
-    output = run_microtest(join_asset_path('test_filtering'))
+    output = run_microtest_as_module(join_asset_path('test_filtering'))
     assert 'config executed' in output
     assert 'slow_test' not in output
     assert 'normal_test' in output
@@ -55,7 +56,7 @@ def test_exclude_groups():
 @microtest.test
 def test_only_groups():
     os.environ['MICROTEST_ENTRYPOINT'] = 'only_slow.py'
-    output = run_microtest(join_asset_path('test_filtering'))
+    output = run_microtest_as_module(join_asset_path('test_filtering'))
     assert 'config executed' in output
     assert 'slow_test' in output
     assert 'normal_test' not in output
